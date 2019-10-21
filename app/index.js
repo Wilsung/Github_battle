9e4ebe84592b3f46a400b13539acf3e98290a4b0
Wilson Ung <wou123@yahoo.com>
2019-10-20 22:51:09 -0700

    added query-string

--- app/index.js
+++ app/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 import Test from './components/test'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import Results from './components/Results'
 import './index.css'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component{
     constructor(props){
@@ -22,14 +24,18 @@ class App extends React.Component{
     }
     render(){
         return(
-            <ThemeProvider value={this.state}>
-                <div className={this.state.theme}>
-                    <div className="container">
-                        <Nav />
-                        <Battle />
+            <Router>
+                <ThemeProvider value={this.state}>
+                    <div className={this.state.theme}>
+                        <div className="container">
+                            <Nav />
+                            <Route exact path="/" component={Popular}/>
+                            <Route exact path ='/battle' component={Battle}/>
+                            <Route path ='/battle/results' component={Results}/>
+                        </div>
                     </div>
-                </div>
-            </ThemeProvider>
+                </ThemeProvider>
+            </Router>
         )
     }
 }
